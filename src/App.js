28dfe76ff5c6aa64f212c9af80cbd4cf80590337
Aswini Sreeja <aswinisreeja.j@gmail.com>
2024-07-09 10:12:29 +0530

    git commit -m

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Header from "./Header";
+import Welcome from"./class components";
 import LifecycleDemo from "./lifecycle";
 function App() {
   return (
     <div >
       <LifecycleDemo/>
       <h1><Header/></h1>
+      <h1><Welcome/></h1>
     </div>
+    
   );
 }
 
